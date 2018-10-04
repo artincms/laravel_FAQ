51b61e205188de2ba5b6ae0800dc139237df8ab6
adel <adel.raheli@gmail.com>
2018-10-05 00:36:57 +0330

    config change and fixed issue

--- src/Config/FAQ.php
+++ src/Config/FAQ.php
@@ -3,11 +3,11 @@
 return [
 
     /* Important Settings */
-    'backend_faq_middlewares'   => explode(',', env('BACKEND_FAQ_MIDDLEWARES', 'web')),
-    'frontend_faq_middlewares'  => explode(',', env('FRONTEND_FAQ_MIDDLEWARES', 'web')),
+    'backend_faq_middlewares'   => explode(',', env('FAQ_BACKEND_MIDDLEWARES', 'web')),
+    'frontend_faq_middlewares'  => explode(',', env('FAQ_FRONTEND_MIDDLEWARES', 'web')),
     // you can change default route from sms-admin to anything you want
-    'backend_faq_route_prefix'  => env('BACKEND_FAQ_ROUTE_PERFIX', 'FAQ'),
-    'frontend_faq_route_prefix' => env('FRONTEND_FAQ_ROUTE_PERFIX', 'FAQ'),
+    'backend_faq_route_prefix'  => env('FAQ_BACKEND_ROUTE_PERFIX', 'FAQ'),
+    'frontend_faq_route_prefix' => env('FAQ_FRONTEND_ROUTE_PERFIX', 'FAQ'),
     // ======================================================================
     //allow user to upload private file in filemanager
     'user_model'                => env('FAQ_USER_MODEL', 'App\User'),
