36361069958c74951e626648c259dbe75bfff3c1
adel <adel.raheli@gmail.com>
2018-10-05 00:21:27 +0330

    middleware .env as array supported

--- src/Config/FAQ.php
+++ src/Config/FAQ.php
@@ -3,16 +3,15 @@
 return [
 
     /* Important Settings */
-    'backend_faq_middlewares' => env('BACKEND_FAQ_MIDDLEWARES', 'web'),
-    'frontend_faq_middlewares' =>  env('FRONTEND_FAQ_MIDDLEWARES', 'web'),
+    'backend_faq_middlewares'   => explode(',', env('BACKEND_FAQ_MIDDLEWARES', 'web')),
+    'frontend_faq_middlewares'  => explode(',', env('FRONTEND_FAQ_MIDDLEWARES', 'web')),
     // you can change default route from sms-admin to anything you want
-    'backend_faq_route_prefix' => env('BACKEND_FAQ_ROUTE_PERFIX', 'FAQ'),
-    'frontend_faq_route_prefix' =>env('FRONTEND_FAQ_ROUTE_PERFIX', 'FAQ'),
+    'backend_faq_route_prefix'  => env('BACKEND_FAQ_ROUTE_PERFIX', 'FAQ'),
+    'frontend_faq_route_prefix' => env('FRONTEND_FAQ_ROUTE_PERFIX', 'FAQ'),
     // ======================================================================
     //allow user to upload private file in filemanager
     'user_model'                => env('FAQ_USER_MODEL', 'App\User'),
     'multi_lang'                => env('FAQ_MULTI_LANG', 'faq_sampleLang'),
-
 
 
 ];
